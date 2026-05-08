7ffa15881b2c570c7b17c21e5aa309eef5288c57
王俐涵 <zzkuaikuai@gmail.com>
2026-05-08 20:08:04 +0800

    任務三修正

--- services/productService.js
+++ services/productService.js
@@ -78,7 +78,7 @@ function displayProducts(products) {
     console.log(`${index + 1}. ${product.title}`);
     console.log(`分類：${product.category}`);
     console.log(`原價：${formatCurrency(product.origin_price)}`);
-    console.log(`售價：${formatCurrency(product.origin_price)} (${getDiscountRate(product)})`);
+    console.log(`售價：${formatCurrency(product.price)} (${getDiscountRate(product)})`);
     console.log('----------------------------------------');
   });
 }
